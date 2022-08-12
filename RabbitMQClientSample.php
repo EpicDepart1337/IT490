936c544eb2af1a706409183b8b5bfcac5c7341c5
EpicDepart1337 <47003644+EpicDepart1337@users.noreply.github.com>
2022-08-11 23:58:32 -0400

    Client update

--- RabbitMQClientSample.php
+++ RabbitMQClientSample.php
@@ -5,10 +5,31 @@ require_once('rabbitMQLib.inc');
 
 $client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
 if(isset($argv[1])){
-	$msg = $argv[1];
+        if(isset($argv[3])){
+                if(strcmp($argv[3], 'register')==0){
+                        $msg = array("user"=>$argv[1], "pass"=>$argv[2], "type"=>"register");
+}
+                                elseif(strcmp($argv[3], 'login')==0){
+                                         $msg = array("user"=>$argv[1], "pass"=>$argv[2], "type"=>"login");
+
+                                }
+}
+else
+{
+         if(strcmp($argv[1], 'multiping')==0)
+        {
+                printf("Testing multiping \n");
+
+                $msg = array("message"=>'test other server', "type"=>"multiping");
+        }
+
+
+
+        $msg = $argv[1];
+}
 }
 else{
-	$msg = array("message"=>"test message", "type"=>"echo");
+        $msg = array("message"=>"test message", "type"=>"echo");
 }
 
 $response = $client->send_request($msg);
